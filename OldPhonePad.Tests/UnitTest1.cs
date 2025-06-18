2974ded19386231a7f4ebed0d7c1d5067faea894
saw mon <sawmon71293@gmail.com>
2025-06-18 17:59:32 +0700

    tests and program

---
+++ OldPhonePad.Tests/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿namespace OldPhonePad.Tests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using OldPhonePad;
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestBasicSequence()
+    {
+        string input = "222#";
+        string expected = "C";
+        string actual = Program.OldPhonePad(input);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void TestWithSpacesAndMultipleLetters()
+    {
+        string input = "2 22 222#";
+        string expected = "ABC";
+        string actual = Program.OldPhonePad(input);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void TestBackspace()
+    {
+        string input = "8 88777444666*6649999#";
+        string expected = "TURINNGZ";
+        string actual = Program.OldPhonePad(input);
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void TestNoHashMeansEmpty()
+    {
+        string input = "222";
+        string expected = "";
+        string actual = Program.OldPhonePad(input);
+        Assert.AreEqual(expected, actual);
+    }
+}
