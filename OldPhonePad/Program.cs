b968684e1d8596ed09121d0f9dbc840754d8d9c1
Saw Mon Han <sawmon71293@gmail.com>
2025-11-01 09:45:08 +0630

    code refactoring

--- OldPhonePad/Program.cs
+++ OldPhonePad/Program.cs
@@ -8,76 +8,158 @@ namespace OldPhonePad
     {
         static void Main(string[] args)
         {
-            string input = "222*2#";
+            // string input = "4433555 555666#";
+            string input = "8 88777444666*664#";
             string result = OldPhonePad(input);
             Console.WriteLine(result);
         }
 
+        // public static string OldPhonePad(string input)
+        // {
+        //     if (string.IsNullOrEmpty(input))
+        //         return string.Empty;
+
+        //     char[] digits = input.ToCharArray();
+
+        //     var keypad = new Dictionary<char, string>
+        //     {
+        //         {'2', "ABC" },
+        //         {'3', "DEF" },
+        //         {'4', "GHI" },
+        //         {'5', "JKL" },
+        //         {'6', "MNO" },
+        //         {'7', "PQRS" },
+        //         {'8', "TUV" },
+        //         {'9', "WXYZ" }
+        //     };
+
+        //     StringBuilder result = new StringBuilder();
+        //     List<char> series = new List<char>();
+
+        //     if (digits != null && digits[digits.Length - 1] != '#')
+        //     {
+        //         return result.ToString(); // return early if # is not typed
+        //     }
+
+        //     for (int i = 0; i < digits.Length; i++)
+        //     {
+        //         char digit = digits[i];
+        //         bool hasNext = (i + 1) < digits.Length;
+        //         char nextDigit = hasNext ? digits[i + 1] : '\0'; // for not to have index out of bound error
+
+        //         if (digit == '#')
+        //         {
+        //             return result.ToString();
+        //         }
+
+        //         else if (digit == ' ')
+        //         {
+        //             continue;
+        //         }
+
+        //         else if (nextDigit == digit && series.Count <= keypad[digit].Length - 1)
+        //         {
+        //             if (i + 2 < digits.Length && digits[i + 2] != '*') // not deleting then add
+        //                 series.Add(digit);
+        //         }
+
+        //         else
+        //         {
+        //             int letterIndex = series.Count;
+        //             if (keypad.ContainsKey(digit))
+        //             {
+        //                 result.Append(keypad[digit][letterIndex]);
+        //             }
+        //             series.Clear();
+        //         }
+
+        //     }
+        //     return result.ToString();
+        // }
+
         public static string OldPhonePad(string input)
+{
+    if (string.IsNullOrWhiteSpace(input))
+        return string.Empty;
+
+    var keypad = new Dictionary<char, string>
+    {
+        {'2', "ABC"},
+        {'3', "DEF"},
+        {'4', "GHI"},
+        {'5', "JKL"},
+        {'6', "MNO"},
+        {'7', "PQRS"},
+        {'8', "TUV"},
+        {'9', "WXYZ"}
+    };
+
+    var result = new StringBuilder();
+    char lastDigit = '\0';
+    int pressCount = 0;
+
+    foreach (char c in input)
+    {
+        switch (c)
         {
-            if (string.IsNullOrEmpty(input))
-                return string.Empty;
+            case '#':
+                // End of input
+                goto End;
 
-            char[] digits = input.ToCharArray();
+            case '*':
+                // Backspace
+                if (result.Length > 0)
+                    result.Remove(result.Length - 1, 1);
+                lastDigit = '\0';
+                pressCount = 0;
+                break;
 
-            var characters = new Dictionary<char, string>
-        {
-            {'2', "ABC" },
-            {'3', "DEF" },
-            {'4', "GHI" },
-            {'5', "JKL" },
-            {'6', "MNO" },
-            {'7', "PQRS" },
-            {'8', "TUV" },
-            {'9', "WXYZ" }
-        };
-
-            StringBuilder result = new StringBuilder();
-            List<char> series = new List<char>();
-
-            if (digits != null && digits[digits.Length - 1] != '#')
-            {
-                return result.ToString(); // return early if # is not typed
-            }
-
-
-            for (int i = 0; i < digits.Length; i++)
-            {
-                char digit = digits[i];
-                bool hasNext = (i + 1) < digits.Length;
-                char nextDigit = hasNext ? digits[i + 1] : '\0'; // for not to have index out of bound error
-
-                if (digit == '#')
+            case ' ':
+                // Pause (finalize previous letter)
+                if (keypad.ContainsKey(lastDigit))
                 {
-                    return result.ToString();
+                    string letters = keypad[lastDigit];
+                    int index = (pressCount - 1) % letters.Length;
+                    result.Append(letters[index]);
                 }
+                lastDigit = '\0';
+                pressCount = 0;
+                break;
 
-                else if (digit == ' ')
+            default:
+                if (keypad.ContainsKey(c))
                 {
-                    continue;
-                }
-
-                else if (nextDigit == digit && series.Count <= characters[digit].Length - 1)
-                {
-                    if (i + 2 < digits.Length && digits[i + 2] != '*') // not deleting then add
-                        series.Add(digit);
-                }
-
-                else
-                {
-                    int letterIndex = series.Count;
-                    if (characters.ContainsKey(digit))
+                    if (c == lastDigit)
                     {
-                        result.Append(characters[digit][letterIndex]);
+                        pressCount++;
                     }
-                    series.Clear();
+                    else
+                    {
+                        if (keypad.ContainsKey(lastDigit))
+                        {
+                            string letters = keypad[lastDigit];
+                            int index = (pressCount - 1) % letters.Length;
+                            result.Append(letters[index]);
+                        }
+                        lastDigit = c;
+                        pressCount = 1;
+                    }
                 }
-
-            }
-
-
-            return result.ToString();
+                break;
         }
+    }
+
+End:
+    if (keypad.ContainsKey(lastDigit))
+    {
+        string letters = keypad[lastDigit];
+        int index = (pressCount - 1) % letters.Length;
+        result.Append(letters[index]);
+    }
+
+    return result.ToString();
+}
+
     }
 
 }
