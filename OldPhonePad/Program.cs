5881e3f28bd8fc4a6a75b4d86bd8e1fe4350112b
Saw Mon Han <sawmon71293@gmail.com>
2025-11-01 09:55:58 +0630

    format

--- OldPhonePad/Program.cs
+++ OldPhonePad/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Text;
 using System.Collections.Generic;
+using System.Text;
 
 namespace OldPhonePad
 {
@@ -78,88 +78,86 @@ namespace OldPhonePad
         // }
 
         public static string OldPhonePad(string input)
-{
-    if (string.IsNullOrWhiteSpace(input))
-        return string.Empty;
-
-    var keypad = new Dictionary<char, string>
-    {
-        {'2', "ABC"},
-        {'3', "DEF"},
-        {'4', "GHI"},
-        {'5', "JKL"},
-        {'6', "MNO"},
-        {'7', "PQRS"},
-        {'8', "TUV"},
-        {'9', "WXYZ"}
-    };
-
-    var result = new StringBuilder();
-    char lastDigit = '\0';
-    int pressCount = 0;
-
-    foreach (char c in input)
-    {
-        switch (c)
         {
-            case '#':
-                // End of input
-                goto End;
+            if (string.IsNullOrWhiteSpace(input))
+                return string.Empty;
 
-            case '*':
-                // Backspace
-                if (result.Length > 0)
-                    result.Remove(result.Length - 1, 1);
-                lastDigit = '\0';
-                pressCount = 0;
-                break;
+            var keypad = new Dictionary<char, string>
+            {
+                { '2', "ABC" },
+                { '3', "DEF" },
+                { '4', "GHI" },
+                { '5', "JKL" },
+                { '6', "MNO" },
+                { '7', "PQRS" },
+                { '8', "TUV" },
+                { '9', "WXYZ" },
+            };
 
-            case ' ':
-                // Pause (finalize previous letter)
-                if (keypad.ContainsKey(lastDigit))
+            var result = new StringBuilder();
+            char lastDigit = '\0';
+            int pressCount = 0;
+
+            foreach (char c in input)
+            {
+                switch (c)
                 {
-                    string letters = keypad[lastDigit];
-                    int index = (pressCount - 1) % letters.Length;
-                    result.Append(letters[index]);
-                }
-                lastDigit = '\0';
-                pressCount = 0;
-                break;
+                    case '#':
+                        // End of input
+                        goto End;
 
-            default:
-                if (keypad.ContainsKey(c))
-                {
-                    if (c == lastDigit)
-                    {
-                        pressCount++;
-                    }
-                    else
-                    {
+                    case '*':
+                        // Backspace
+                        if (result.Length > 0)
+                            result.Remove(result.Length - 1, 1);
+                        lastDigit = '\0';
+                        pressCount = 0;
+                        break;
+
+                    case ' ':
+                        // Pause (finalize previous letter)
                         if (keypad.ContainsKey(lastDigit))
                         {
                             string letters = keypad[lastDigit];
                             int index = (pressCount - 1) % letters.Length;
                             result.Append(letters[index]);
                         }
-                        lastDigit = c;
-                        pressCount = 1;
-                    }
+                        lastDigit = '\0';
+                        pressCount = 0;
+                        break;
+
+                    default:
+                        if (keypad.ContainsKey(c))
+                        {
+                            if (c == lastDigit)
+                            {
+                                pressCount++;
+                            }
+                            else
+                            {
+                                if (keypad.ContainsKey(lastDigit))
+                                {
+                                    string letters = keypad[lastDigit];
+                                    int index = (pressCount - 1) % letters.Length;
+                                    result.Append(letters[index]);
+                                }
+                                lastDigit = c;
+                                pressCount = 1;
+                            }
+                        }
+                        break;
                 }
-                break;
+            }
+
+            End:
+            if (keypad.ContainsKey(lastDigit))
+            {
+                string letters = keypad[lastDigit];
+                int index = (pressCount - 1) % letters.Length;
+                result.Append(letters[index]);
+            }
+
+            return result.ToString();
         }
     }
-
-End:
-    if (keypad.ContainsKey(lastDigit))
-    {
-        string letters = keypad[lastDigit];
-        int index = (pressCount - 1) % letters.Length;
-        result.Append(letters[index]);
-    }
-
-    return result.ToString();
-}
-
-    }
-
 }
